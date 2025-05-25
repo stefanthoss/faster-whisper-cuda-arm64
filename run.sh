e60710c9bcef45d6133f5733ee537c139a98cbbe
Stefan Thoss <stefanthoss@users.noreply.github.com>
2025-05-25 13:50:15 -0700

    Create run.sh

---
+++ run.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+python3 -m wyoming_faster_whisper \
+    --uri 'tcp://0.0.0.0:10300' \
+    --data-dir /data \
+    --download-dir /data \
+    --model ${WHISPER_MODEL} \
+    --language ${WHISPER_LANG} \
+    --beam-size ${WHISPER_BEAM} \
+    --device cuda \
+    --debug "$@"
